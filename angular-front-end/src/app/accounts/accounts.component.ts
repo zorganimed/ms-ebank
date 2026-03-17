313f648dcc261d1020526d43e167d2e5454bb9bd
zorganimed <zorganimed.mohamed@gmail.com>
2026-03-17 20:40:46 +0100

    add front end + environment variables + Install curl for healthcheck dans Dockerfile

---
+++ angular-front-end/src/app/accounts/accounts.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {CommonModule} from "@angular/common";
+
+@Component({
+  selector: 'app-accounts',
+  standalone: true,
+  imports: [HttpClientModule, CommonModule],
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css'
+})
+export class AccountsComponent implements OnInit{
+
+  accounts : any;
+  constructor(private http : HttpClient) {
+  }
+  ngOnInit(): void {
+    this.http.get("http://localhost:8888/ACCOUNT-SERVICE/accounts").subscribe({
+      next : data =>{
+        this.accounts = data;
+      },
+      error : err => {
+        console.log(err)
+      }
+    })
+  }
+
+
+}
